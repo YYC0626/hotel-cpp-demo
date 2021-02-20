52b56e6ecfd32d9a1d6d38cf54bb7e2491ab64d0
YYC0626 <yaoyuchen0626@163.com>
2021-02-20 00:18:12 -0800

    add CMakeLists.txt

---
+++ src/CMakeFiles/PKG_SRC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PKG_SRC.dir/consumer.cpp.o"
+  "CMakeFiles/PKG_SRC.dir/manager.cpp.o"
+  "CMakeFiles/PKG_SRC.dir/person.cpp.o"
+  "CMakeFiles/PKG_SRC.dir/waiter.cpp.o"
+  "libPKG_SRC.a"
+  "libPKG_SRC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PKG_SRC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
